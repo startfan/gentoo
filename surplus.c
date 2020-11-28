9e2f11bb314518e5e43ee5cd66c78d139decdedf
startfan <785715834@qq.com>
2020-11-28 12:20:27 +0800

    sss

--- surplus.c
+++ surplus.c
@@ -1,15 +1,17 @@
 #include<stdio.h>//找出多余的配置项
-#include<string.h>
+#include<string.h>//需要将比较的两个文件交替使用才能获得两个文件所独有的内容
+#define A 100//否则只能获得其中一个文件的独有项
 int main(int argc,char** argv)
 {
-	FILE* a;FILE* b;FILE* c;char d[50];
-	char e[50];
+	FILE* a;FILE* b;FILE* c;char d[A];
+	char e[A];
+	puts("(file a)(file b)(output file)");
 	if(argc!=4)puts("3,Find the excess configuration items");
 	if((a=fopen(argv[1],"r"))==NULL||(b=fopen(argv[2],"r"))==NULL||(c=fopen(argv[3],"w"))==NULL)
 		puts("open error");
-a:	while(fgets(d,50,a)!=NULL){
+a:	while(fgets(d,A,a)!=NULL){
 		rewind(b);//重头读b文件
-		while(fgets(e,50,b)!=NULL){
+		while(fgets(e,A,b)!=NULL){
 			if(strcmp(d,e)==0)goto a;//找到相同内容就读a文件的下一行，否则就读b文件的下一行
 		}
 		fputs(d,c);	
